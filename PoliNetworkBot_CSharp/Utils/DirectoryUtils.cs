c3236fb6c05cd06017682a0bea86e3bddeb8f7f8
Federico Armellini <armellini.federico@gmail.com>
2020-07-31 17:59:27 +0200

    Update DirectoryUtils.cs

---
+++ PoliNetworkBot_CSharp/Utils/DirectoryUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace PoliNetworkBot_CSharp.Utils
+{
+    internal class DirectoryUtils
+    {
+        internal static void CreateDirectory(string v)
+        {
+            try
+            {
+                Directory.CreateDirectory(v);
+            }
+            catch
+            {
+                ;
+            }
+        }
+    }
+}
