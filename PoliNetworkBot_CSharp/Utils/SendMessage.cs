ced8859a4574493013c6e0e4d6c5e98dadf269ef
Federico Armellini <armellini.federico@gmail.com>
2020-07-31 20:42:45 +0200

    Update SendMessage.cs

--- PoliNetworkBot_CSharp/Utils/SendMessage.cs
+++ PoliNetworkBot_CSharp/Utils/SendMessage.cs
@@ -6,9 +6,26 @@ namespace PoliNetworkBot_CSharp.Utils
 {
     internal class SendMessage
     {
-        internal static void SendMessageInPrivateOrAGroup(TelegramBotClient telegramBotClient, MessageEventArgs e, string s1)
+        internal static void SendMessageInPrivateOrAGroup(TelegramBotClient telegramBotClient, MessageEventArgs e, string text)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+                telegramBotClient.SendTextMessageAsync(e.Message.From.Id, text, Telegram.Bot.Types.Enums.ParseMode.Html);
+            }
+            catch
+            {
+                string message_to = GetMessageTo(e);
+                string text2 = "[Messaggio per "+message_to+"]\n\n" + text;
+                telegramBotClient.SendTextMessageAsync(e.Message.Chat.Id, text2);
+            }
+        }
+
+        private static string GetMessageTo(MessageEventArgs e)
+        {
+            string name = e.Message.From.FirstName + " " + e.Message.From.LastName;
+            name = name.Trim();
+            return "<a href=\"tg://user?id="+e.Message.From.Id+"\">"+name+"</a>";
         }
     }
 }
