7dd5ee54bd1529c18c973567341ae9ad96ee4857
Federico Armellini <armellini.federico@gmail.com>
2020-08-25 13:30:05 +0200

    Update contentModel.xml, riderModule.iml, and 110 more files...

---
+++ TestBot/Code/Utils/MessageDb.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace TestBot.Code.Utils
+{
+    public class MessageDb
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
