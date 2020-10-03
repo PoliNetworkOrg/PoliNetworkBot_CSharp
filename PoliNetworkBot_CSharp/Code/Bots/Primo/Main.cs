b53e70318eae1710b294d64e5fbe083599fdd405
armellini.federico@gmail.com <armellini.federico@gmail.com>
2020-10-03 15:12:06 +0200

    Update Main.cs and PoliNetworkBot_CSharp.dll

---
+++ PoliNetworkBot_CSharp/Code/Bots/Primo/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace PoliNetworkBot_CSharp.Code.Bots.Primo
+{
+    class Main
+    {
+        public void MainPrimo()
+        {
+            Thread t = new Thread(MainPrimo2);
+            t.Start();
+        }
+
+        private async void MainPrimo2()
+        {
+            return;
+        }
+    }
+}
