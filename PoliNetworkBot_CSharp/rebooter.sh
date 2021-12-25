26599e6733913377143b138f68d4f027f1e23e14
Eliaxie <elia.maggioni@gmail.com>
2021-12-25 18:35:40 +0100

    .

--- PoliNetworkBot_CSharp/rebooter.sh
+++ PoliNetworkBot_CSharp/rebooter.sh
@@ -3,8 +3,5 @@ cd /home/ubuntu/bot/PoliNetworkBot_CSharp/
 git pull
 echo "rebooting bot"
 cd /home/ubuntu/bot/PoliNetworkBot_CSharp/PoliNetworkBot_CSharp/bin/Debug/net5.0/
-read REPLY
 screen -XS bot_runner quit
-read REPLY
 screen -d -m -S bot_runner dotnet PoliNetworkBot_CSharp.dll 2
-read REPLY
