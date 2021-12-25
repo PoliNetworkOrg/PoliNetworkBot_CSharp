6677bf1fb033eeddbf9cb71d5c2bf490db11ad31
Elia Maggioni <elia.maggioni@gmail.com>
2021-12-25 17:03:03 +0100

    .

--- PoliNetworkBot_CSharp/rebooter.sh
+++ PoliNetworkBot_CSharp/rebooter.sh
@@ -1,6 +1,6 @@
 echo "Pulling changes"
 git pull
 echo "rebooting bot"
-cd PoliNetworkBot_CSharp/bin/Debug/net5.0/ || exit
+cd bin/Debug/net5.0/ || exit
 screen -XS bot_runner quit
 screen -S bot_runner dotnet PoliNetworkBot_CSharp.dll 2
