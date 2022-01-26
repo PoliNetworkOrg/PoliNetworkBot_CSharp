371212e8a7a978f90e483400c2c54e8da73df78a
Eliaxie <elia.maggioni.gm@gmail.com>
2022-01-26 11:33:16 +0100

    Update rebooter.sh

--- PoliNetworkBot_CSharp/rebooter.sh
+++ PoliNetworkBot_CSharp/rebooter.sh
@@ -1,11 +1,10 @@
+echo "killing bot"
+screen -XS bot_runner quit
 echo "Pulling changes"
 cd /home/ubuntu/bot/PoliNetworkBot_CSharp/
-git fetch
-git switch build
+git pull
 echo "rebooting bot"
 cd /home/ubuntu/bot/PoliNetworkBot_CSharp/PoliNetworkBot_CSharp/bin/Debug/net5.0/
-sleep  1
-screen -XS bot_runner quit
 sleep  1
 screen -d -m -S bot_runner dotnet PoliNetworkBot_CSharp.dll 2
 echo "screen started"
