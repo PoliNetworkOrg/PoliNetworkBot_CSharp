a8d8980d5cb57249f0bfc0f0c8ab398c5d5ede2d
Eliaxie <elia.maggioni@gmail.com>
2022-02-23 15:32:46 +0100

    Update GlobalVariables.cs

--- PoliNetworkBot_CSharp/Code/Data/GlobalVariables.cs
+++ PoliNetworkBot_CSharp/Code/Data/GlobalVariables.cs
@@ -49,7 +49,7 @@ namespace PoliNetworkBot_CSharp.Code.Data
             {
                 "carlogiova", "giovannieffe777", "testpolinetwork",
                 "albus25", "deet98", "alberto_fattori", "scala98",
-                "giulia_ye", "andre_crc", "perularrabeiti", "fllippo", "marcol_8", "@andre_crc", "@lucreziaal", "raif9", "diegoaldarese"
+                "giulia_ye", "andre_crc", "perularrabeiti", "fllippo", "marcol_8", "@andre_crc", "@lucreziaal", "@giada_marti", "raif9", "diegoaldarese"
             };
 
             AllowedBanAll = new List<string>
