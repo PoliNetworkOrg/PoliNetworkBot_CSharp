91cf05c202e5081e061aa09532a91c80f21489e2
steven9463912 <steven9463912@gmail.com>
2022-04-12 15:08:58 +0200

    Update PhotoAlbumUploader.cs

--- PoliNetworkBot_CSharp/Code/IG/Minista/Helpers/Uploaders/PhotoAlbumUploader.cs
+++ PoliNetworkBot_CSharp/Code/IG/Minista/Helpers/Uploaders/PhotoAlbumUploader.cs
@@ -1,6 +1,5 @@
 ﻿#region
 
-using InstagramApiSharp.Classes;
 using Minista.Helpers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -28,23 +27,15 @@ internal class PhotoAlbumUploader
 
     public PhotoAlbumUploader()
     {
-        ReportCompleted += PhotoAlbumUploader_ReportCompleted;
     }
 
     private string Caption { get; set; }
-
-    public event EventHandler<string> ReportCompleted;
 
     private static string GenerateUploadId()
     {
         var timeSpan = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0);
         var uploadId = (long)timeSpan.TotalMilliseconds;
         return uploadId.ToString();
-    }
-
-    private static Uri GetMediaConfigureUri()
-    {
-        return new Uri("https://i.instagram.com/api/v1/media/configure_sidecar/", UriKind.RelativeOrAbsolute);
     }
 
     public async Task SetFiles(IEnumerable<StorageFile> files, string caption, InstaApi instaApi)
@@ -62,215 +53,6 @@ internal class PhotoAlbumUploader
             _uploads.Add(uploadId, b);
             await Task.Delay(120);
         }
-    }
-
-    private async void PhotoAlbumUploader_ReportCompleted(object sender, string e)
-    {
-        if (!_uploaded.Contains(e))
-            _uploaded.Add(e);
-
-        if (_uploaded.Count != _uploads.Count) return;
-        await Task.Delay(10000);
-        await ConfigurePhotoAsync();
-    }
-
-    private async Task ConfigurePhotoAsync()
-    {
-        try
-        {
-            Debug.WriteLine(
-                "----------------------------------------ConfigurePhotoAsync----------------------------------");
-            var instaUri = GetMediaConfigureUri();
-            var retryContext = GetRetryContext();
-            var user = instaApi.GetLoggedUser();
-            var deviceInfo = instaApi.GetCurrentDevice();
-            var clientSidecarId = GenerateUploadId();
-            var childrenArray = new JArray();
-
-            foreach (var al in _uploads)
-                //if (al.Value.IsImage)
-                //    childrenArray.Add(_instaApi.HelperProcessor.GetImageConfigure(al.Key, al.Value.ImageToUpload));
-                //else if (al.Value.IsVideo)
-                //    childrenArray.Add(_instaApi.HelperProcessor.GetVideoConfigure(al.Key, al.Value.VideoToUpload));
-
-                childrenArray.Add(GetImageConfigure(al.Key));
-
-            var data = new JObject
-            {
-                { "_uuid", deviceInfo.DeviceGuid.ToString() },
-                { "_uid", user.LoggedInUser.Pk.ToString() },
-                { "_csrftoken", user.CsrfToken },
-                { "caption", Caption },
-                { "client_sidecar_id", clientSidecarId },
-                { "upload_id", clientSidecarId },
-                { "timezone_offset", InstaApi.GetTimezoneOffset() },
-                { "source_type", "4" },
-                { "device_id", deviceInfo.DeviceId },
-                { "creation_logger_session_id", Guid.NewGuid().ToString() },
-                {
-                    "device", new JObject
-                    {
-                        { "manufacturer", deviceInfo.HardwareManufacturer },
-                        { "model", deviceInfo.DeviceModelIdentifier },
-                        { "android_release", deviceInfo.AndroidVer.VersionNumber },
-                        { "android_version", int.Parse(deviceInfo.AndroidVer.APILevel) }
-                    }
-                },
-                { "children_metadata", childrenArray }
-            };
-            //if (location != null)
-            //{
-            //    data.Add("location", location.GetJson());
-            //    data.Add("date_time_digitalized", DateTime.Now.ToString("yyyy:dd:MM+h:mm:ss"));
-            //}
-
-            var httpRequestProcessor = instaApi.HttpRequestProcessor;
-            var request = instaApi.HttpHelper.GetSignedRequest(instaUri, deviceInfo, data);
-            request.Headers.Add("retry_context", GetRetryContext());
-            var response = await httpRequestProcessor.SendAsync(request);
-            var json = await response.Content.ReadAsStringAsync();
-
-            //SendCompleted();
-            //IsUploading = false;
-            Result.Success(true);
-        }
-        catch (HttpRequestException httpException)
-        {
-            Result.Fail(httpException, false, ResponseType.NetworkProblem);
-        }
-        catch (Exception exception)
-        {
-            Result.Fail<bool>(exception);
-        }
-    }
-
-    private JObject GetImageConfigure(string uploadId /*,InstaImageUpload image*/)
-    {
-        var deviceInfo = instaApi.GetCurrentDevice();
-        var imgData = new JObject
-        {
-            { "timezone_offset", InstaApi.GetTimezoneOffset() },
-            { "source_type", "4" },
-            { "upload_id", uploadId },
-            { "caption", "" },
-            {
-                "extra", JsonConvert.SerializeObject(new JObject
-                {
-                    { "source_width", 0 },
-                    { "source_height", 0 }
-                })
-            },
-            {
-                "device", JsonConvert.SerializeObject(new JObject
-                {
-                    { "manufacturer", deviceInfo.HardwareManufacturer },
-                    { "model", deviceInfo.DeviceModelIdentifier },
-                    { "android_release", deviceInfo.AndroidVer.VersionNumber },
-                    { "android_version", deviceInfo.AndroidVer.APILevel }
-                })
-            }
-        };
-        //if (image.UserTags?.Count > 0)
-        //{
-        //    var tagArr = new JArray();
-        //    foreach (var tag in image.UserTags)
-        //    {
-        //        if (tag.Pk != -1)
-        //        {
-        //            var position = new JArray(tag.X, tag.Y);
-        //            var singleTag = new JObject
-        //                        {
-        //                            {"user_id", tag.Pk},
-        //                            {"position", position}
-        //                        };
-        //            tagArr.Add(singleTag);
-        //        }
-        //    }
-
-        //    var root = new JObject
-        //    {
-        //        {"in",  tagArr}
-        //    };
-        //    imgData.Add("usertags", root.ToString(Formatting.None));
-        //}
-        return imgData;
-    }
-
-    /*
-    public JObject GetVideoConfigure(string uploadId, InstaVideoUpload video)
-    {
-        var vidData = new JObject
-        {
-            {"timezone_offset", InstaApiConstants.TIMEZONE_OFFSET.ToString()},
-            {"caption", ""},
-            {"upload_id", uploadId},
-            {"date_time_original", DateTime.Now.ToString("yyyy-dd-MMTh:mm:ss-0fffZ")},
-            {"source_type", "4"},
-            {
-                "extra", JsonConvert.SerializeObject(new JObject
-                {
-                    {"source_width", 0},
-                    {"source_height", 0}
-                })
-            },
-            {
-                "clips", JsonConvert.SerializeObject(new JArray{
-                    new JObject
-                    {
-                        {"length", video.Video.Length},
-                        {"source_type", "4"},
-                    }
-                })
-            },
-            {
-                "device", JsonConvert.SerializeObject(new JObject{
-                    {"manufacturer", _deviceInfo.HardwareManufacturer},
-                    {"model", _deviceInfo.DeviceModelIdentifier},
-                    {"android_release", _deviceInfo.AndroidVer.VersionNumber},
-                    {"android_version", _deviceInfo.AndroidVer.APILevel}
-                })
-            },
-            {"length", video.Video.Length.ToString()},
-            {"poster_frame_index", "0"},
-            {"audio_muted", "false"},
-            {"filter_type", "0"},
-            {"video_result", ""},
-        };
-        if (video.UserTags?.Count > 0)
-        {
-            var tagArr = new JArray();
-            foreach (var tag in video.UserTags)
-            {
-                if (tag.Pk != -1)
-                {
-                    var position = new JArray(0.0, 0.0);
-                    var singleTag = new JObject
-                    {
-                        {"user_id", tag.Pk},
-                        {"position", position}
-                    };
-                    tagArr.Add(singleTag);
-                }
-            }
-
-            var root = new JObject
-            {
-                {"in",  tagArr}
-            };
-            vidData.Add("usertags", root.ToString(Formatting.None));
-        }
-        return vidData;
-    }
-    */
-
-    private static string GetRetryContext()
-    {
-        return new JObject
-        {
-            { "num_step_auto_retry", 0 },
-            { "num_reupload", 0 },
-            { "num_step_manual_retry", 0 }
-        }.ToString(Formatting.None);
     }
 }
 
