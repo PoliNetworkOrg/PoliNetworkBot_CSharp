760ce07c54b4bfc9f8fbdd23c1a29a084dc65a42
steven9463912 <steven9463912@gmail.com>
2022-08-02 11:21:55 +0200

    don't send everytime the ram size but only if there is a significant change

--- PoliNetworkBot_CSharp/Code/Utils/RamSize.cs
+++ PoliNetworkBot_CSharp/Code/Utils/RamSize.cs
@@ -16,31 +16,70 @@ namespace PoliNetworkBot_CSharp.Code.Utils;
 
 public static class RamSize
 {
+    private static RamUsed? _ramUsedStatic;
+
     [SuppressMessage("ReSharper", "FunctionNeverReturns")]
     public static async Task CheckRamSizeThread()
     {
         while (true)
         {
-            try
-            {
-                var proc = Process.GetCurrentProcess();
-                var ramUsed1 = proc.PrivateMemorySize64;
-                var ramUsed2 = GC.GetTotalMemory(true);
-                var message = "Ram size " + ramUsed1 + " " + ramUsed2;
-                Logger.Logger.WriteLine(message);
-                await SendMessage.SendMessageInAGroup(BotUtil.GetFirstModerationRealBot(), "en",
-                    new Language(
-                        new Dictionary<string, string?>
-                        {
-                            { "en", message }
-                        }), null, Data.Constants.Groups.BackupGroup, ChatType.Group, ParseMode.Html, null, true);
-            }
-            catch (Exception? ex)
-            {
-                Logger.Logger.WriteLine(ex, LogSeverityLevel.ERROR);
-            }
-
+            await CheckRamSizeThread2();
             Thread.Sleep(1000 * 60 * 60 * 12); // every 12 hours
         }
+    }
+
+    private static async Task CheckRamSizeThread2()
+    {
+        try
+        {
+            var proc = Process.GetCurrentProcess();
+            var procPrivateMemorySize64 = proc.PrivateMemorySize64;
+            var ramUsed2 = GC.GetTotalMemory(true);
+            var ramUsed = new RamUsed(procPrivateMemorySize64, ramUsed2);
+            var firstTime = _ramUsedStatic == null;
+            _ramUsedStatic ??= ramUsed;
+
+            if (!_ramUsedStatic.InferioreDi(ramUsed) && firstTime == false)
+            {
+                _ramUsedStatic = ramUsed;
+                return;
+            }
+
+            _ramUsedStatic = ramUsed;
+            var message = "Ram size " + ramUsed;
+            Logger.Logger.WriteLine(message);
+            await SendMessage.SendMessageInAGroup(BotUtil.GetFirstModerationRealBot(), "en",
+                new Language(
+                    new Dictionary<string, string?>
+                    {
+                        { "en", message }
+                    }), null, Data.Constants.Groups.BackupGroup, ChatType.Group, ParseMode.Html, null, true);
+        }
+        catch (Exception? ex)
+        {
+            Logger.Logger.WriteLine(ex, LogSeverityLevel.ERROR);
+        }
+    }
+}
+
+internal class RamUsed
+{
+    private readonly long _ram1;
+    private readonly long _ram2;
+
+    public RamUsed(long ramUsed1, long ramUsed2)
+    {
+        _ram1 = ramUsed1;
+        _ram2 = ramUsed2;
+    }
+
+    public override string ToString()
+    {
+        return _ram1 + " " + _ram2;
+    }
+
+    public bool InferioreDi(RamUsed ramUsed)
+    {
+        return _ram1 * 1.2d < ramUsed._ram1 || _ram2 * 1.2d < ramUsed._ram2;
     }
 }
