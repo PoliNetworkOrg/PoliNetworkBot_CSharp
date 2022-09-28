e68246b54be459c4319cc4770fe0454e52240d56
steven9463912 <steven9463912@gmail.com>
2022-09-28 15:07:34 +0200

    Update InviteLinks.cs and GroupsFixLog.cs

--- PoliNetworkBot_CSharp/Code/Utils/Logger/GroupsFixLog.cs
+++ PoliNetworkBot_CSharp/Code/Utils/Logger/GroupsFixLog.cs
@@ -50,7 +50,8 @@ public static class GroupsFixLog
 
         var escaped = HttpUtility.HtmlEncode(message);
         Logger.WriteLine(message);
-        NotifyUtil.NotifyOwners_AnError_AndLog3(escaped, telegramBotAbstract, messageEventArgs, FileTypeJsonEnum.SIMPLE_STRING);
+        NotifyUtil.NotifyOwners_AnError_AndLog3(escaped, telegramBotAbstract, messageEventArgs,
+            FileTypeJsonEnum.SIMPLE_STRING);
         Reset();
     }
 
