822da71ac44c2584f50c2cd3f91bdd7da031efdf
user@example.com <user@example.com>
2022-11-24 00:47:59 +0100

    Update Main.cs

--- PoliNetworkBot_CSharp/Code/Bots/Administration/Main.cs
+++ PoliNetworkBot_CSharp/Code/Bots/Administration/Main.cs
@@ -6,7 +6,6 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using PoliNetworkBot_CSharp.Code.Objects;
 using PoliNetworkBot_CSharp.Code.Objects.TelegramBotAbstract;
 using PoliNetworkBot_CSharp.Code.Utils;
 using PoliNetworkBot_CSharp.Code.Utils.Logger;
