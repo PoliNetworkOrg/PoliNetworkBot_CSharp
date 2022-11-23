bada234f10e394e0af0f75cc89b3445199002126
user@example.com <user@example.com>
2022-11-24 00:48:31 +0100

    Update BotUtil.cs and ConfigUtil.cs

--- PoliNetworkBot_CSharp/Code/Utils/ConfigUtil.cs
+++ PoliNetworkBot_CSharp/Code/Utils/ConfigUtil.cs
@@ -6,6 +6,7 @@ using PoliNetworkBot_CSharp.Code.Enums;
 using PoliNetworkBot_CSharp.Code.Objects;
 using PoliNetworkBot_CSharp.Code.Objects.TelegramBotAbstract;
 using PoliNetworkBot_CSharp.Code.Objects.TelegramMedia;
+using PoliNetworkBot_CSharp.Code.Utils.Main;
 using Telegram.Bot.Types.Enums;
 
 #endregion
@@ -17,7 +18,7 @@ public static class ConfigUtil
     public static bool GetConfig(long? fromId, string? fromUsername, TelegramBotAbstract? sender,
         string? fromLanguageCode, ChatType? chatType)
     {
-        var json = JsonConvert.SerializeObject(Main.ProgramUtil.BotConfigAll);
+        var json = JsonConvert.SerializeObject(ProgramUtil.BotConfigAll);
         var file = TelegramFile.FromString(json, "config.json", "");
         if (chatType == null)
             return false;
