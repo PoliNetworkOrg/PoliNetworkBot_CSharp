471801fb07ba81d3eb6186b8307ad80ed3206385
Eliaxie <elia.maggioni@gmail.com>
2022-11-30 11:56:37 +0100

    removed user from rebooter

--- PoliNetworkBot_CSharp/static/rebooter.sh
+++ PoliNetworkBot_CSharp/static/rebooter.sh
@@ -1,10 +1,10 @@
 echo "killing bot"
 screen -XS bot_runner quit
 echo "Pulling changes"
-cd /home/ubuntu/bot/PoliNetworkBot_CSharp/
+cd ~/bot/PoliNetworkBot_CSharp/
 git pull --allow-unrelated-histories -f -X theirs --no-edit --rebase
 echo "rebooting bot"
-cd /home/ubuntu/bot/PoliNetworkBot_CSharp/
+cd ~/bot/PoliNetworkBot_CSharp/
 sleep  1
 screen -d -m -S bot_runner dotnet PoliNetworkBot_CSharp.dll 2
 echo "screen started"
