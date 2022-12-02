c693a8f0c00992e841e7c0864feaa6307574e0bd
user@example.com <user@example.com>
2022-12-03 00:30:29 +0100

    find files

--- PoliNetworkBot_CSharp/Code/Utils/FileUtils/FileUtil.cs
+++ PoliNetworkBot_CSharp/Code/Utils/FileUtils/FileUtil.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Linq;
 
 namespace PoliNetworkBot_CSharp.Code.Utils.FileUtils;
 
@@ -9,17 +9,52 @@ public static class FileUtil
     {
         while (count < 10)
         {
-  
-
-
-            string?[] files = Directory.GetFiles(startingPath, "*" + dbJson, SearchOption.AllDirectories);
-            if (files.Length > 1)
+            var files = FindFiles(startingPath,  dbJson);
+            if (!string.IsNullOrEmpty(files))
             {
-                return files[0];
+                return files;
             }
 
             startingPath += "../";
             count += 1;
+        }
+
+        return null;
+    }
+
+    private static string? FindFiles(string startingPath, string dbJson)
+    {
+        try
+        {
+            var files = Directory.EnumerateFiles(startingPath).Where(x => x.EndsWith(dbJson)).ToList();
+            if (files.Any())
+                return files[0];
+        }
+        catch
+        {
+            return null;
+        }
+
+        try
+        {
+            var folders = Directory.EnumerateDirectories(startingPath);
+            foreach (var f in folders)
+            {
+                try
+                {
+                    var x2 = FindFiles(f, dbJson);
+                    if (!string.IsNullOrEmpty(x2))
+                        return x2;
+                }
+                catch
+                {
+                    ;
+                }
+            }
+        }
+        catch
+        {
+            ;
         }
 
         return null;
