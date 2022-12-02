e8a5ba3a6f6b900eab15607b013de854caa86637
user@example.com <user@example.com>
2022-12-02 23:45:55 +0100

    restore db

---
+++ PoliNetworkBot_CSharp/Code/Utils/FileUtils/FileUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace PoliNetworkBot_CSharp.Code.Utils.FileUtils;
+
+public static class FileUtil
+{
+    public static string? FindFile(string dbJson, string startingPath = "./", int count = 0)
+    {
+        while (count < 10)
+        {
+  
+
+
+            string?[] files = Directory.GetFiles(startingPath, "*" + dbJson, SearchOption.AllDirectories);
+            if (files.Length > 1)
+            {
+                return files[0];
+            }
+
+            startingPath += "../";
+            count += 1;
+        }
+
+        return null;
+    }
+}
