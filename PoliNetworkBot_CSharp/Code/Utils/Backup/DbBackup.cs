33d0614234504690f66431ed69e1b5cf7caaa618
user@example.com <user@example.com>
2022-12-03 17:40:21 +0100

    refactor

--- PoliNetworkBot_CSharp/Code/Utils/Backup/DbBackup.cs
+++ PoliNetworkBot_CSharp/Code/Utils/Backup/DbBackup.cs
@@ -14,44 +14,7 @@ public static class DbBackup
         {
             DB_Backup db = new();
 
-            const string? q = "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA='polinetwork';";
-            if (telegramBotAbstract == null) return JsonConvert.SerializeObject(db);
-            var r = Database.ExecuteSelect(q, telegramBotAbstract.DbConfig);
-            if (r == null)
-                return JsonConvert.SerializeObject("ERROR 1");
-
-            try
-            {
-                var tableNames = r.Rows.Cast<DataRow>().ToList();
-
-                var c1 = tableNames.Where(row => row is { ItemArray.Length: > 0 } && row.ItemArray[0] != null)
-                    .Select(row =>
-                    {
-                        var argItem = row.ItemArray[0];
-                        return argItem != null ? argItem.ToString() : "";
-                    });
-
-                foreach (var c3 in c1)
-                    if (!string.IsNullOrEmpty(c3))
-                        db.tableNames.Add(c3);
-
-                foreach (var tableName in
-                         db.tableNames.Where(tableName => string.IsNullOrEmpty(tableName) == false))
-                    try
-                    {
-                        var q2 = "SELECT * FROM " + tableName;
-                        var r2 = Database.ExecuteSelect(q2, telegramBotAbstract.DbConfig);
-                        if (r2 != null) db.tables[tableName] = r2;
-                    }
-                    catch
-                    {
-                        // ignored
-                    }
-            }
-            catch
-            {
-                // ignored
-            }
+            FillTables(db, telegramBotAbstract);
 
             return JsonConvert.SerializeObject(db);
         }
@@ -61,5 +24,47 @@ public static class DbBackup
         }
 
         return JsonConvert.SerializeObject("ERROR 2");
+    }
+
+    private static void FillTables(DB_Backup db, TelegramBotAbstract? telegramBotAbstract)
+    {
+        const string? q = "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA='polinetwork';";
+        if (telegramBotAbstract == null) return;
+
+        var r = Database.ExecuteSelect(q, telegramBotAbstract.DbConfig);
+        if (r == null) return;
+
+        try
+        {
+            var tableNames = r.Rows.Cast<DataRow>().ToList();
+
+            var c1 = tableNames.Where(row => row is { ItemArray.Length: > 0 } && row.ItemArray[0] != null)
+                .Select(row =>
+                {
+                    var argItem = row.ItemArray[0];
+                    return argItem != null ? argItem.ToString() : "";
+                });
+
+            foreach (var c3 in c1)
+                if (!string.IsNullOrEmpty(c3))
+                    db.tableNames.Add(c3);
+
+            foreach (var tableName in
+                     db.tableNames.Where(tableName => string.IsNullOrEmpty(tableName) == false))
+                try
+                {
+                    var q2 = "SELECT * FROM " + tableName;
+                    var r2 = Database.ExecuteSelect(q2, telegramBotAbstract.DbConfig);
+                    if (r2 != null) db.tables[tableName] = r2;
+                }
+                catch
+                {
+                    // ignored
+                }
+        }
+        catch
+        {
+            // ignored
+        }
     }
 }
