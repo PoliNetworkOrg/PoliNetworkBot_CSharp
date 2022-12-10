5b92583d23847ff513c7f1866b71c5ce35a368e6
user@example.com <user@example.com>
2022-12-10 14:55:35 +0100

    check spam

---
+++ TestBot/Code/Bots/Moderation/Spam/CheckSpam.cs
@@ -0,0 +1,15 @@
+﻿namespace TestBot.Code.Bots.Moderation.Spam;
+
+public class CheckSpam
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
