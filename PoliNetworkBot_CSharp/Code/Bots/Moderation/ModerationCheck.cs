8e48cb47ad40f5c8176d22a52c2991f7f1a35c90
user@example.com <user@example.com>
2022-12-10 15:09:09 +0100

    check spam (2)

--- PoliNetworkBot_CSharp/Code/Bots/Moderation/ModerationCheck.cs
+++ PoliNetworkBot_CSharp/Code/Bots/Moderation/ModerationCheck.cs
@@ -343,13 +343,15 @@ internal static class ModerationCheck
             return false;
 
         var from1 = message.From;
-        var chat = message.Chat;
-        return chat.Type == ChatType.Private || (from1 != null && Innocuo(from1, chat));
+        Chat? chat = message.Chat;
+
+        var b = chat?.Type == ChatType.Private;
+        return b || (from1 != null && Innocuo(from1, chat));
     }
 
-    private static bool Innocuo(User from1, Chat chat)
+    private static bool Innocuo(User from1, Chat? chat)
     {
-        return from1.Id == 777000 || from1.Id == chat.Id ||
+        return from1.Id == 777000 || from1.Id == chat?.Id ||
                CheckIfIsInList(GlobalVariables.AllowedSpam, from1) ||
                CheckIfIsInList(GlobalVariables.Creators, from1) ||
                CheckIfIsInList(GlobalVariables.SubCreators, from1) ||
