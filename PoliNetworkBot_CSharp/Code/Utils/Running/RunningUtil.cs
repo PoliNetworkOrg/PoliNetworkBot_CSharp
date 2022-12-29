738cc1f88c519b7a7265c33adabc0884ae341549
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2022-12-29 20:35:13 +0100

    kill_yourself

---
+++ PoliNetworkBot_CSharp/Code/Utils/Running/RunningUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using PoliNetworkBot_CSharp.Code.Objects;
+using PoliNetworkBot_CSharp.Code.Objects.TelegramBotAbstract;
+
+namespace PoliNetworkBot_CSharp.Code.Utils.Running;
+
+public static class RunningUtil
+{
+    public static Task<CommandExecutionState> KillYourself(MessageEventArgs? arg1, TelegramBotAbstract? arg2, string[]? arg3)
+    {
+        try
+        {
+            Environment.Exit(0);
+        }
+        catch
+        {
+            ;
+        }
+
+        return Task.FromResult(CommandExecutionState.SUCCESSFUL);
+    }
+}
