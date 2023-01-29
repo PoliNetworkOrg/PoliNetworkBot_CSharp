a6e1fb537599244664cf54309da9d80810b73d37
Elia Maggioni <elia.maggioni@gmail.com>
2023-01-29 20:24:16 +0100

    cloner + pusher

--- PoliNetworkBot_CSharp/static/github_cloner.sh
+++ PoliNetworkBot_CSharp/static/github_cloner.sh
@@ -4,4 +4,5 @@ cd ./data || exit
 eval "$(ssh-agent -s)"
 ssh-add /git/ssh-key
 git clone git@github.com:PoliNetworkDev/polinetworkWebsiteData.git
+cd ./polinetworkWebsiteData
 git remote add org https://github.com/PoliNetworkOrg/polinetworkWebsiteData.git
