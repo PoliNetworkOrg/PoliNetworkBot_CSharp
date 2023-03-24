2347a431077ebf22ea7e16bfe6628ce3fb8718f6
Eliaxie <elia.maggioni@gmail.com>
2023-03-24 17:55:46 +0100

    Fetcher

--- PoliNetworkBot_CSharp/Code/Bots/RoomsBot/Fetcher.cs
+++ PoliNetworkBot_CSharp/Code/Bots/RoomsBot/Fetcher.cs
@@ -9,25 +9,57 @@ namespace PoliNetworkBot_CSharp.Code.Bots.RoomsBot;
 public class Fetcher
 {
  
-    private static TimeSpan _cacheInvalidationTime = TimeSpan.FromHours(1);
-    private static List<string> _freeClassroomsCache = new();
-    private static Dictionary<string, DateTime> _fetchCacheAge = new();
-    private static Dictionary<string, HtmlDocument> _rawFetchedFile = new();
-    private static Dictionary<string, Dictionary<string, bool>> _occupancyPerCampus = new();
-    private static Dictionary<string, DateTime> _occupancyCacheAge = new();
+    private static readonly TimeSpan CacheInvalidationTime = TimeSpan.FromHours(1);
+    private static readonly Dictionary<string, DateTime> _fetchCacheAge = new();
+    private static readonly Dictionary<string, HtmlDocument> _rawFetchedFile = new();
 
     public static string? GetRawOccupancy(string campus)
     {
         
         var parsedCampus = Data.Enums.GetCampusByName(campus);
-        var doc = FetchData(parsedCampus);
+        var doc = FetchOccupationData(parsedCampus);
         var parsedDoc = doc.DocumentNode.SelectNodes("//table[contains(@class, 'BoxInfoCard')]");
         return parsedDoc?.ToString();
     }
 
-    private static HtmlDocument FetchData(string campus)
+    private static List<string> GetFreeClassrooms(string campus, int startingTime, int endingTime)
     {
-        if (DateTime.Now - _fetchCacheAge[campus] < _cacheInvalidationTime)
+        var parsedCampus = Data.Enums.GetCampusByName(campus);
+        var doc = FetchOccupationData(parsedCampus);
+        var classroomRows = doc.DocumentNode.SelectNodes("//tr[contains(@class, 'normalRow')]");
+        var toReturn = new List<string>();
+        foreach (var row in classroomRows)
+        {
+            var freeClassroom = true;
+            for (var i = 2 + startingTime - 8; i < row.OuterLength && i < 2 + endingTime - 8; i++)
+            {
+                if (!row.ChildNodes[i].HasClass("slot")) continue;
+                freeClassroom = false;
+                break;
+            }
+            if (!freeClassroom) continue;
+            var roomName = row.ChildNodes[1].InnerText;
+            toReturn.Add(roomName);
+        }
+
+        return toReturn;
+    }
+
+    public List<string> GetAllClassrooms(string campus)
+    {
+        return GetFreeClassrooms(campus, 8, 8);
+    }
+
+    public string? GetSingleClassroom(string campus, string roomName)
+    {
+        var parsedCampus = Data.Enums.GetCampusByName(campus);
+        var doc = FetchOccupationData(parsedCampus);
+        return doc.DocumentNode.SelectSingleNode($"//div[.='{roomName}']")?.ToString();
+    }
+
+    private static HtmlDocument FetchOccupationData(string campus)
+    {
+        if (DateTime.Now - _fetchCacheAge[campus] < CacheInvalidationTime)
             return _rawFetchedFile[campus];
         var doc = new HtmlDocument();
         doc.LoadHtml(Const.PolimiController + $"/?csic={campus}?tipologia={Data.Enums.RoomType.tutte}?categoria=tutte" );
