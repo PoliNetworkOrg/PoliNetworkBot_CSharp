2d4b49e4bb357c073b4674365306f4dd7a86a22a
Eliaxie <elia.maggioni@gmail.com>
2023-03-24 14:40:13 +0100

    Init aule

---
+++ PoliNetworkBot_CSharp/Code/Bots/AuleBot/Main.cs
@@ -0,0 +1,46 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using PoliNetworkBot_CSharp.Code.Enums;
+using PoliNetworkBot_CSharp.Code.Objects;
+using PoliNetworkBot_CSharp.Code.Objects.TelegramBotAbstract;
+using PoliNetworkBot_CSharp.Code.Utils;
+using PoliNetworkBot_CSharp.Code.Utils.CallbackUtils;
+using PoliNetworkBot_CSharp.Code.Utils.Logger;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+#endregion
+
+namespace PoliNetworkBot_CSharp.Code.Bots.AuleBot;
+
+internal static class AuleBot
+{
+    internal static void MainMethod(object? sender, MessageEventArgs? e)
+    {
+        var t = new Thread(() =>
+        {
+            if (sender != null) _ = MainMethod2(sender, e);
+        });
+        t.Start();
+    }
+
+    private static async Task MainMethod2(object sender, MessageEventArgs? e)
+    {
+        if (e?.Message == null)
+            return;
+
+        if (e.Message.Chat.Type != ChatType.Private) return;
+
+        if (sender is not TelegramBotClient t2)
+            return;
+
+        var telegramBotAbstract = TelegramBotAbstract.GetFromRam(t2);
+
+    }
+
+}
