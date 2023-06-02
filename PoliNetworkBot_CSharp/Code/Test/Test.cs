a367ed3378ddd75ea2d50c81a245d9c94233972f
github.nouser@nouser.github.com <github.nouser@nouser.github.com>
2023-06-03 01:21:49 +0200

    Update Test.cs

--- PoliNetworkBot_CSharp/Code/Test/Test.cs
+++ PoliNetworkBot_CSharp/Code/Test/Test.cs
@@ -5,6 +5,11 @@ namespace PoliNetworkBot_CSharp.Code.Test;
 
 internal static class Test
 {
+    public static void TestRun()
+    {
+        _ = MainTest();
+    }
+    
     public static async Task MainTest()
     {
         await RestoreDbUtil.RestoreDb();
